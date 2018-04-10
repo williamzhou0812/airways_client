b6c3d4c495a2eb71bf1e72f148b47ac6b9d45860
Neal Shen <>
2018-04-10 12:53:14 +1000

    added apartment list

--- src/components/apartments/Apartments.js
+++ src/components/apartments/Apartments.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import './Apartments.css';
+import _ from 'lodash';
+import { createImageURL } from '../utils/Constants';
 
 class Apartments extends Component {
     constructor(props) {
@@ -12,6 +14,33 @@ class Apartments extends Component {
         console.log(this.props.apartmentsList.apartments);
     }
 
+    renderEachApartment() {
+        return _.map(
+            this.props.apartmentsList.apartments.concat(
+                this.props.apartmentsList.apartments
+            ),
+            ({ name, images_path }) => {
+                return (
+                    <div
+                        className="aptsection--list--each"
+                        style={{
+                            backgroundImage: `url(${createImageURL(
+                                images_path[0]
+                            )})`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center'
+                        }}
+                    >
+                        <div className="aptsection--list--each--name">
+                            <span>{name}</span>
+                        </div>
+                    </div>
+                );
+            }
+        );
+    }
+
     render() {
         return (
             <div
@@ -20,9 +49,13 @@ class Apartments extends Component {
             >
                 <div className="apt--container">
                     <div className="aptsection aptsection--sidebar">
-                        <p className="aptsection--sidebar--text">APARTMENTS</p>
+                        <p>APARTMENTS</p>
                     </div>
-                    <div className="aptsection aptsection--main">main</div>
+                    <div className="aptsection  ">
+                        <div className=" aptsection--list">
+                            {this.renderEachApartment()}
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/components/utils/Constants.js
+++ src/components/utils/Constants.js
@@ -1,7 +1,12 @@
-const HOST = '192.168.10.10';
+const SERVER_HOST = 'airways_server.test';
+const CMS_HOST = 'airways_cms.test';
 
 function createURL(namespace) {
-    return 'http://' + HOST + '/' + namespace;
+    return 'http://' + SERVER_HOST + '/' + namespace;
 }
 
-export { createURL };
+function createImageURL(namespace) {
+    return 'http://' + CMS_HOST + namespace;
+}
+
+export { createURL, createImageURL };
