a255e6767597189a28d74d4b2039205040f20c7a
Neal Shen <>
2018-04-13 10:24:08 +1000

    added gallery list

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import './Gallery.css';
+import _ from 'lodash';
+import { createImageURL } from '../utils/Constants';
 
 class Gallery extends Component {
     constructor(props) {
@@ -12,13 +14,39 @@ class Gallery extends Component {
         console.log(this.props.galleryList.gallery);
     }
 
+    renderThumbnailItem() {
+        return _.map(this.props.galleryList.gallery, item => {
+            return (
+                <div
+                    className="thumbnail--item"
+                    style={
+                        !_.isEmpty(item.images_path) && {
+                            backgroundImage: `url(${createImageURL(
+                                item.images_path
+                            )})`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center'
+                        }
+                    }
+                />
+            );
+        });
+    }
+
     render() {
         return (
-            <div
-                className="main-section-animation"
-                style={{ backgroundColor: 'green' }}
-            >
-                <p>Gallery</p>
+            <div className="main-section-animation">
+                <div className="gallery--container">
+                    <div className="galleryitem galleryitem--image">
+                        1: Orange
+                    </div>
+                    <div className="galleryitem galleryitem--list">
+                        <div className="list--container">
+                            {this.renderThumbnailItem()}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
