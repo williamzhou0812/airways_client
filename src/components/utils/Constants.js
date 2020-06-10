ef260985d6a344ecbb05d31b6ba530f509be241e
williamzhou0812 <54008865+williamzhou0812@users.noreply.github.com>
2020-06-10 15:00:35 +1000

    first commit

--- src/components/utils/Constants.js
+++ src/components/utils/Constants.js
@@ -1,7 +1,12 @@
 //const SERVER_HOST = "api.jbgconcierge.com.au";
 //const CMS_HOST = "cms.jbgconcierge.com.au";
-const SERVER_HOST = "airways_server.test";
-const CMS_HOST = "airways_cms.test";
+// const SERVER_HOST = "airways_server.test";
+// const CMS_HOST = "airways_cms.test";
+// const SERVER_HOST = "127.0.0.1:8005";
+// const CMS_HOST = "127.0.0.1:8000";
+// const IDLE_TIME = 100000;
+const SERVER_HOST = "13.236.207.155/airways_server/public";
+const CMS_HOST = "13.236.207.155/airways_cms/public";
 const IDLE_TIME = 100000;
 
 function createURL(namespace) {
